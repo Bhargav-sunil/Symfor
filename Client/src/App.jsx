97e09acb9459669ddce30f0af4bff27a12042ea8
Bhargav-sunil <bhargavsunil2166@gmail.com>
2025-09-26 14:13:19 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Category from "./components/category/Category";
+import Category from "./components/Category/Category";
 import "./App.css";
 import Dishes from "./components/Dishes/Dishes";
 
